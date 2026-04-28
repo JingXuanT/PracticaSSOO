0c82e9b4bf3837b813f51319480619f778396d38
Ubuntu <azureuser@SSOO.gjoyg5nrvitenpkxexa0rsshdf.madx.internal.cloudapp.net>
2026-04-28 16:20:24 +0200

    Añadido puntos y ganador

--- venv/servidor.py
+++ venv/servidor.py
@@ -6,10 +6,10 @@ app= Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins ="*")
 
 partidas ={}
-CATEGORIAS_BASE =['nombre', 'apellido', 'animal', 'comida', 'fruta', 'color', 'pais', 'objeto']
+CATEGORIAS_BASE =['nombre', 'apellido', 'animal', 'comida', 'color', 'pais', 'objeto']
 
 def temporizador_partida(room):
-    socketio.sleep(90)
+    socketio.sleep(60)
     if room in partidas and partidas[room]['activa']:
         finalizador_partidas(room, "¡Tiempo agotado!")
 
@@ -19,23 +19,33 @@ def desbloquear_categoria_auto(room, cat):
         del partidas[room]['bloqueos'][cat]
         socketio.emit('desbloquear_categoria', {'cat': cat}, room= room)
 
-def finalizador_partidas(room,razon):
+def finalizador_partidas(room, razon):
     if room in partidas:
-        partidas[room]['activa'] = False
-        socketio.emit('fin_juego', {'razon': razon, 'tablero':partidas[room]['categorias']}, room=room)
+        partida = partidas[room]
+        ganador_sid = max(partida['puntos'], key=partida['puntos'].get)
+        puntos_max = partida['puntos'][ganador_sid]
+        nombre_ganador = partida['nombres'].get(ganador_sid, "Desconocido")
+        resultado = f"{razon} | GANADOR: {nombre_ganador} con {puntos_max} puntos."
+        socketio.emit('fin_juego', {
+            'razon': resultado,
+            'tablero': partida['categorias'],
+            'puntos': partida['puntos'],
+            'nombres': partida['nombres']
+        }, room=room)
 
 @socketio.on('iniciar_juego')
 def iniciar(data):
     room = str(data['room'])
     letra_elegida = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    partidas[room] = {
-        'letra': letra_elegida,
-        'categorias': {cat: "" for cat in CATEGORIAS_BASE},
-        'activa': True,
-        'bloqueos': {}
-     }
+    partida = partidas.get(room)
+    if not partida:
+       return
+    partida['letra'] = letra_elegida
+    partida['categorias'] = {cat: "" for cat in CATEGORIAS_BASE}
+    partida['activa'] = True
+    partida['bloqueos'] = {}
 
-    emit('juego_iniciado', {'letra': letra_elegida, 'tiempo': 90}, room=room)
+    emit('juego_iniciado', {'letra': letra_elegida, 'tiempo': 60}, room=room)
     socketio.start_background_task(temporizador_partida, room)
 
 @socketio.on('palabra_lista')
@@ -51,6 +61,8 @@ def recibir_palabra(data):
     letra_juego = partida['letra']
     if palabra.startswith(letra_juego):
         partida['categorias'][cat] = palabra
+        partida['puntos'][request.sid] += 1
+        partida['categorias'][cat] = palabra
         if cat in partida['bloqueos']: del partida['bloqueos'][cat]
 
         emit('actualizar_tablero', {'cat': cat, 'val': palabra}, room=room)
@@ -62,6 +74,10 @@ def recibir_palabra(data):
         emit('error', {
             'msg': f'La palabra debe empezar por la letra {letra_juego}'})
 
+    if palabra.startswith(letra_juego):
+       if partida['categorias'][cat] == "":
+          partida['puntos'][request.sid] += 1
+          partida['categorias'][cat] = palabra
 
 @app.route('/stop/<int:game_id>')
 def index(game_id):
@@ -76,12 +92,26 @@ def handle_join(data):
         'categorias': {cat: "" for cat in CATEGORIAS_BASE},
         'bloqueos':{},
         'jugadores': set(),
-        'activa':False
+	'nombres':{},
+        'activa':False,
+	'puntos': {}
 }
+
+    if request.sid not in partidas[room]['nombres']:
+        numero = len(partidas[room]['nombres']) + 1
+        nombre_asignado = f"Jugador {numero}"
+        partidas[room]['nombres'][request.sid] = nombre_asignado
+        partidas[room]['puntos'][request.sid] = 0
+    emit('asignar_nombre', {'nombre' : nombre_asignado}, room=request.sid)
+    if 'puntos' not in partidas[room]:
+      partidas[room]['partidas'] = {}
+
+    partidas[room]['puntos'][request.sid] = 0
+
     MAX_JUGADORES = 10
     if len(partidas[room]['jugadores']) >= MAX_JUGADORES:
-    	emit('error', {'msg': f'La sala {room} está llena. Máximo {MAX_JUGADORES} jugadores.'})
-    	return
+        emit('error', {'msg': f'La sala {room} está llena. Máximo {MAX_JUGADORES} jugadores.'})
+        return
 
     join_room(room)
     partidas[room]['jugadores'].add(request.sid)
@@ -112,7 +142,9 @@ def crear_partida():
             'activa': False,
             'categorias': { cat: "" for cat in CATEGORIAS_BASE},
             'jugadores': set(),
-            'bloqueos': {}
+            'bloqueos': {},
+            'nombres': {},
+            'puntos': {}
     }
     return redirect(url_for('index', game_id=game_id))
 
